f04b1158140641147e7093ea13b0f95809146a81
Sharp Hall <sharp@sharphall.org>
2024-08-03 13:48:53 -0400

    Allow multiple ports, multiple containers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,22 +4,43 @@
 	import { yaml as yamlLang } from 'svelte-highlight/languages/yaml';
 	let appName = '';
 	let appNamePlaceholder = 'nginx';
-	let image = '';
 	let imagePlaceholder = 'nginx:latest';
-	let yamlStr = 'HELLO';
+	let replicas: string | number = '';
+	let yamlStr = '';
+	type Container = {
+		image: string;
+		ports: (number | string)[];
+	};
+	const emptyContainer = {
+		image: '',
+		ports: []
+	};
+	let containers: Container[] = [
+		{
+			image: '',
+			ports: [80]
+		}
+	];
+
+	function enforceNumber(num: number | string) {
+		return (typeof num == 'string' && isFinite(parseInt(num as string))) ||
+			(typeof num == 'number' && isFinite(num))
+			? parseInt(num as string)
+			: '';
+	}
 
 	function buildYaml() {
-		let resource = {
+		let resource: any = {
 			apiVersion: 'apps/v1',
 			kind: 'Deployment',
 			metadata: {
-				name: 'nginx-deployment',
+				name: appName || appNamePlaceholder,
 				labels: {
-					app: 'nginx'
+					app: appName || appNamePlaceholder
 				}
 			},
 			spec: {
-				replicas: 3,
+				replicas,
 				selector: {
 					matchLabels: {
 						app: appName || appNamePlaceholder
@@ -30,40 +51,103 @@
 						labels: { app: appName || appNamePlaceholder }
 					},
 					spec: {
-						containers: [
-							{
-								name: appName || appNamePlaceholder,
-								image: image || imagePlaceholder,
-								ports: [{ containerPort: 80 }]
-							}
-						]
+						containers: containers.map((container) => ({
+							name: appName || appNamePlaceholder,
+							image: container.image || imagePlaceholder,
+							ports: container.ports.map((p) => ({
+								containerPort: p
+							}))
+						}))
 					}
 				}
 			}
 		};
+		if (resource.spec.replicas === 1 || resource.spec.replicas === '') {
+			delete resource.spec['replicas'];
+		}
 		yamlStr = yaml.dump(resource);
 	}
 
 	$: {
 		appName;
-		image;
+		replicas = enforceNumber(replicas);
+		containers.forEach((container) => {
+			container.ports = container.ports.map(enforceNumber);
+		});
 		buildYaml();
 	}
 </script>
 
-<label class="label">
-	<span>Name</span>
-	<input
-		class="input"
-		type="text"
-		placeholder={appNamePlaceholder}
-		name="name"
-		bind:value={appName}
-	/>
-</label>
-<label class="label">
-	<span>Image</span>
-	<input class="input" type="text" placeholder={imagePlaceholder} name="image" bind:value={image} />
-</label>
-<button type="button" class="btn variant-filled">Copy to clipboard</button>
+<div class="deployment m-8">
+	<label class="label">
+		<span>Name</span>
+		<input
+			class="input"
+			type="text"
+			placeholder={appNamePlaceholder}
+			name="name"
+			bind:value={appName}
+		/>
+	</label>
+	<label class="label">
+		<span>Replicas</span>
+		<input class="input" type="text" placeholder="1" name="name" bind:value={replicas} />
+	</label>
+	{#each containers as container, container_idx}
+		<div class="k-container card p-4 mt-4 mb-4">
+			<h2>Container</h2>
+			<label class="label">
+				<span>Image</span>
+				<input
+					class="input"
+					type="text"
+					placeholder={imagePlaceholder}
+					name="image"
+					bind:value={container.image}
+				/>
+			</label>
+			{#each container.ports as port, idx}
+				<label class="label flex items-center">
+					<span class="flex-none label">Port #{idx + 1}</span>
+					<input class="input grow" type="text" bind:value={port} />
+					<button
+						type="button"
+						class="btn variant-filled flex-none"
+						on:click={() =>
+							(container.ports = [
+								...container.ports.slice(0, idx),
+								...container.ports.slice(idx + 1)
+							])}>Remove</button
+					>
+				</label>
+			{/each}
+			<button
+				type="button"
+				class="btn variant-filled flex-none"
+				on:click={() => (container.ports = [...container.ports, 80])}>Add port</button
+			>
+			{#if container_idx > 0}
+				<button
+					type="button"
+					class="btn variant-filled flex-none"
+					on:click={() =>
+						(containers = [
+							...containers.slice(0, container_idx),
+							...containers.slice(container_idx + 1)
+						])}>Remove container</button
+				>
+			{/if}
+		</div>
+	{/each}
+	<button
+		type="button"
+		class="btn variant-filled flex-none"
+		on:click={() => (containers = [...containers, { ...emptyContainer }])}>Add container</button
+	>
+</div>
+<button
+	type="button"
+	class="btn variant-filled"
+	on:click={() => navigator.clipboard.writeText(yamlStr)}>Copy to clipboard</button
+>
 <Highlight language={yamlLang} code={yamlStr} />
