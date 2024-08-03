93e30fbf8f9000a1cf216058020372d9d1720150
Sharp Hall <sharp@sharphall.org>
2024-08-03 13:57:09 -0400

    Set github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -36,7 +36,7 @@
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/sharph"
+					href="https://github.com/sharph/k8sgen/"
 					target="_blank"
 					rel="noreferrer"
 				>
